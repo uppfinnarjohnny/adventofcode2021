65198afb2e93bf52354c2c4aaab7f876729d44bc
Johnny Karhinen <johnny@karhinen.se>
2021-12-02 00:31:57 +0100

    Better naming, cleaner code

--- day1.py
+++ day1.py
@@ -1,10 +1,10 @@
 values = list(map(int, open('input1.txt').readlines()))
 
-def count_increases(v):
-    return sum(map(lambda vs: int(vs[0] > vs[1]), zip(v + [v[-1]], [v[0]] + v)))
+def count_increases(values):
+    return sum(int(pair[0] > pair[1]) for pair in zip(values + [values[-1]], [values[0]] + values))
 
-def triplet_sums(v):
-    return [sum(vs) for vs in zip(v, v[1:], v[2:])]
+def triplet_sums(values):
+    return [sum(triplet) for triplet in zip(values, values[1:], values[2:])]
 
 print("Day 1 part 1:", count_increases(values))
 print("Day 1 part 2:", count_increases(triplet_sums(values)))
