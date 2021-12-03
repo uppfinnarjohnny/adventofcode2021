a86c99f33780b701c8959f91cf446e5d35139d1b
Johnny Karhinen <johnny@karhinen.se>
2021-12-03 10:45:58 +0100

    Day 3 part 2!

--- day3.py
+++ day3.py
@@ -1,6 +1,6 @@
 lines = list(open('input3.txt').readlines())
 
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 def part1(lines):
     counts = defaultdict(int)
@@ -29,4 +29,47 @@ def part1(lines):
 
     return gamma * epsilon
 
+
 print("Day 3 part 1:", part1(lines))
+
+def find_most_common_at_index(lines, index, on_draw):
+    at_index = [l[index] for l in lines]
+    result = Counter(at_index).most_common()
+
+    if len(result) == 1:
+        most_common = result[0][0]
+    elif result[0][1] == result[1][1]:
+        most_common = on_draw
+    else:
+        most_common = result[0][0]
+    
+    return most_common
+
+def part2(lines):
+    oxygen = co2 = 0
+
+    oxygen_index = 0
+    oxygen_filtered = lines
+
+    while len(oxygen_filtered) != 1:
+        most_common_at_index = find_most_common_at_index(oxygen_filtered, oxygen_index, '1')
+        oxygen_filtered = [l for l in oxygen_filtered if l[oxygen_index] == most_common_at_index]
+        oxygen_index += 1
+
+    oxygen = int(oxygen_filtered[0], 2)
+    
+    co2_index = 0
+    co2_filtered = lines
+    
+    while len(co2_filtered) != 1:
+        least_common_at_index = '1' if find_most_common_at_index(co2_filtered, co2_index, '1') == '0' else '0'
+
+        co2_filtered = [l for l in co2_filtered if l[co2_index] == least_common_at_index]
+        co2_index += 1
+
+    co2 = int(co2_filtered[0], 2)
+
+    return oxygen * co2
+
+
+print("Day 3 part 2:", part2(lines))
