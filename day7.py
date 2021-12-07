6b5b17f664d411012a956224e106fb6e6f8eee9f
Johnny Karhinen <johnny@karhinen.se>
2021-12-07 08:06:48 +0100

    Cleanup of day 7

--- day7.py
+++ day7.py
@@ -1,22 +1,21 @@
+from functools import partial
+
 submarine_positions = [int(l) for l in open('input7.txt').read().split(',')]
 
-def part2_fuel(distance):
-    return distance * (distance + 1) / 2
 
-def fuel_consumption_for_positions(positions, new_position):
-    return sum(abs(p - new_position) for p in positions)
+def fuel_consumption_for_positions(positions, new_position, consumption_function):
+    return sum(consumption_function(abs(p - new_position)) for p in positions)
 
 
-def part2_fuel_consumption_for_positions(positions, new_position):
-    return sum(part2_fuel(abs(p - new_position)) for p in positions)
+part1_fuel_calculator = partial(fuel_consumption_for_positions, consumption_function=lambda d: d)
+part2_fuel_calculator = partial(fuel_consumption_for_positions, consumption_function=lambda d: d * (d + 1) / 2)
 
 
-def find_lowest_consumption(positions):
-    lowest_consumption_position = None
-    lowest_consumption = None
+def find_lowest_consumption(positions, fuel_calculator):
+    lowest_consumption = lowest_consumption_position = None
 
     for new_position in range(min(submarine_positions), max(submarine_positions) + 1):
-        consumption = fuel_consumption_for_positions(positions, new_position)
+        consumption = fuel_calculator(positions, new_position)
 
         if lowest_consumption is None or consumption < lowest_consumption:
             lowest_consumption = consumption
@@ -25,20 +24,5 @@ def find_lowest_consumption(positions):
     return lowest_consumption, lowest_consumption_position
 
 
-def part2_find_lowest_consumption(positions):
-    lowest_consumption_position = None
-    lowest_consumption = None
-
-    for new_position in range(min(submarine_positions), max(submarine_positions) + 1):
-        consumption = part2_fuel_consumption_for_positions(positions, new_position)
-
-        if lowest_consumption is None or consumption < lowest_consumption:
-            lowest_consumption = consumption
-            lowest_consumption_position = new_position
-    
-    return lowest_consumption, lowest_consumption_position
-
-
-assert fuel_consumption_for_positions([16,1,2,0,4,2,7,1,2,14], 2) == 37
-print("Day 7 part 1:", find_lowest_consumption(submarine_positions))
-print("Day 7 part 2:", part2_find_lowest_consumption(submarine_positions))
+print("Day 7 part 1:", find_lowest_consumption(submarine_positions, part1_fuel_calculator))
+print("Day 7 part 2:", find_lowest_consumption(submarine_positions, part2_fuel_calculator))
