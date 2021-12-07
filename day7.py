377ff11bc92ae83bd56399a858a18e1f0288e28b
Johnny Karhinen <johnny@karhinen.se>
2021-12-07 07:40:45 +0100

    Quick and dirty day 7 part 2

--- day7.py
+++ day7.py
@@ -1,8 +1,15 @@
 submarine_positions = [int(l) for l in open('input7.txt').read().split(',')]
 
+def part2_fuel(distance):
+    return sum(range(distance + 1))
+
 
 def fuel_consumption_for_positions(positions, new_position):
     return sum(abs(p - new_position) for p in positions)
+
+
+def part2_fuel_consumption_for_positions(positions, new_position):
+    return sum(part2_fuel(abs(p - new_position)) for p in positions)
 
 
 def find_lowest_consumption(positions):
@@ -19,5 +26,20 @@ def find_lowest_consumption(positions):
     return lowest_consumption, lowest_consumption_position
 
 
+def part2_find_lowest_consumption(positions):
+    lowest_consumption_position = None
+    lowest_consumption = None
+
+    for new_position in range(min(submarine_positions), max(submarine_positions) + 1):
+        consumption = part2_fuel_consumption_for_positions(positions, new_position)
+
+        if lowest_consumption is None or consumption < lowest_consumption:
+            lowest_consumption = consumption
+            lowest_consumption_position = new_position
+    
+    return lowest_consumption, lowest_consumption_position
+
+
 assert fuel_consumption_for_positions([16,1,2,0,4,2,7,1,2,14], 2) == 37
 print("Day 7 part 1:", find_lowest_consumption(submarine_positions))
+print("Day 7 part 2:", part2_find_lowest_consumption(submarine_positions))
